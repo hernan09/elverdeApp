d784acb356ec3c2ff578d8e12e74be074f3bbafb
hegomez <hegomez@telecentro.net.ar>
2023-02-21 18:25:58 -0300

    grafico

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import './App.css';
 import Home from './Components/Home/main.jsx';
 //import Footer from './Components/Footer/footer.jsx';
-import Particles from "react-tsparticles";
-import particlesConfig from './config/particles';
-import { useCallback } from "react";
-import { loadFull } from "tsparticles";
+// import Particles from "react-tsparticles";
+// import particlesConfig from './config/particles';
+// import { useCallback } from "react";
+// import { loadFull } from "tsparticles";
 
 
 
 function App() {
-  const particlesInit = useCallback(async engine => {
-    await loadFull(engine);
-}, []);
+//   const particlesInit = useCallback(async engine => {
+//     await loadFull(engine);
+// }, []);
 
-const particlesLoaded = useCallback(async container => {
-    await console.log(container);
-}, []);
+// const particlesLoaded = useCallback(async container => {
+//     await console.log(container);
+// }, []);
   
   
   return (
     <div className="App">
-     <Particles options={particlesConfig} init={particlesInit} loaded={particlesLoaded}></Particles>
+     {/* <Particles options={particlesConfig} init={particlesInit} loaded={particlesLoaded}></Particles> */}
      <Home></Home>
      {/* <Footer></Footer> */}
     </div>
--- src/Components/Home/main.jsx
+++ src/Components/Home/main.jsx
@@ -56,8 +56,8 @@ function Home(){
             setDolarOfi(json[0].source); // etiqueta Oficial
             setShellOfi(json[0].value_sell); // valor de venta oficial
             setBuyOfi(json[0].value_buy); // valor de compra oficial
-            calculateAverageBlue(ShellBLueAfter, ShellBlue);
-            calculateAverageOficial(ShellOfiAfter, ShellOfi)
+            calculateAverageBlue(buyBlue, ShellBlue);
+            calculateAverageOficial(buyOfi, ShellOfi)
             CalculateGrowthBlue()
             setIsLoading(false);
         })
@@ -180,7 +180,7 @@ function Home(){
                         <span className='item-span'>{buyBlue}</span>
                     </div>
                     <div className='content-text promedio blue-average'>
-                        <h3 className='title-prom'>Promedio 1 dia</h3>
+                        <h3 className='title-prom'>Promedio</h3>
                         <span className='item-span-prom'>{averageBlue}</span>
                         <span className='content-arrow-blue'>
                             {flagBlue && <img className='img-arrow' src={ArrowUp} alt='...'/>}
@@ -202,7 +202,7 @@ function Home(){
                         <span className='item-span'>{buyOfi}</span>
                     </div>  
                     <div className='content-text promedio oficial-average'>
-                        <h3 className='title-prom'>Promedio 1 dia</h3>
+                        <h3 className='title-prom'>Promedio</h3>
                         <span className='item-span-prom'>{averageOfi}</span>
                         <span className='content-arrow-ofi'>
                             {flagOfi && <img className='img-arrow' src={ArrowUp} alt='...'/>}
